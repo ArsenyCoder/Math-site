adb717d129acf441a9f88a9840bdd3459bcc7a8a
ArsenyCoder <114302368+ArsenyCoder@users.noreply.github.com>
2022-10-21 16:58:13 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -42,32 +42,12 @@ $('#little-about-me-is').on('click',function(e) {
   alert('Я-репетитор');
 });
 
-
-var body = document.getElementsByTagName('body')[0];
-var bodyScrollTop = null;
-var locked = false;
-
-function lockScroll(){
-  if (!locked) {
-    bodyScrollTop = (typeof window.pageYOffset !== 'undefined') ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    body.classList.add('scroll-locked');
-    body.style.top = `-${bodyScrollTop}px`;
-    locked = true;
-  };
-}
-function unlockScroll(){
-  if (locked) {
-    body.classList.remove('scroll-locked');
-    body.style.top = null;
-    window.scrollTo(0, bodyScrollTop);
-    locked = false;
-  }
-}
+$('#Popup').on('shown.bs.modal', function (e) {
+  $('#body').css('touch-action','none');
+  $('#body').css('-ms-touch-action','none');
+});
 
 $('#Popup').on('hidden.bs.modal', function (e) {
-  unlockScroll();
-
-});
-$('#Popup').on('shown.bs.modal', function (e) {
-  lockScroll();
+  $('#body').css('touch-action','');
+  $('#body').css('-ms-touch-action','');
 });
