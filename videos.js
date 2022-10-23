293aeb742b084ea56cc0cb1fc263a96fd19c437e
ArsenyCoder <114302368+ArsenyCoder@users.noreply.github.com>
2022-10-23 19:41:19 +0300

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,21 @@
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('lesson-main-video', {
+    videoId: 'OmJ-4B-mS-Yg',
+    events: {
+    'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function OpenMainVideo() {
+  player.playVideo();
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
