476113fdf7dc4b67e843139b5197205cc6a85e82
ArsenyCoder <114302368+ArsenyCoder@users.noreply.github.com>
2022-10-31 21:33:59 +0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -11,7 +11,7 @@ $("#up-link").click(function(e) {
 });
 
 $('#modal-link').on('click',function(e) {
-  $('body,html').animate({ scrollTop: $('#registration-card').offset().top - ($(window).height() - $('#registration-card').height() + $('#nav').height()) / 2},1500);
+  $('body,html').animate({ scrollTop: $('#registration-card').offset().top - $('#nav').height() - ($(window).height() - $('#registration-card').height() - $('#nav').height()) / 2},1500);
 });
 
 $('#about-me-link').on('click',function(e) {
@@ -21,20 +21,19 @@ $('#about-me-link').on('click',function(e) {
 
 $('#open-video').on('click',function(e) {
   e.preventDefault();
-  $('body,html').animate({ scrollTop: $('#main-video-container').offset().top - ($(window).height() - $('#main-video-container').height() + $('#nav').height()) / 2},1500);
+  $('body,html').animate({ scrollTop: $('#main-video-container').offset().top - $('#nav').height() - ($(window).height() - $('#main-video-container').height() - $('#nav').height()) / 2},1500);
   window.setTimeout(OpenMainVideo, 1500);
 });
 
 $('#open-map-popup').on('click',function(e) {
   e.preventDefault();
-  $('body,html').animate({ scrollTop: $('#main-map-id').offset().top - ($(window).height() - $('#main-map-id').height() + $('#nav').height()) / 2},650);
+  $('body,html').animate({ scrollTop: $('#main-map-id').offset().top - $('#nav').height() - ($(window).height() - $('#main-map-id').height() - $('#nav').height()) / 2},650);
 });
 
 $('#about-me-id-link').on('click',function(e) {
   e.preventDefault();
   location.assign('about.html');
 });
-
 
 $('#little-about-me-is').on('click',function(e) {
   e.preventDefault();
