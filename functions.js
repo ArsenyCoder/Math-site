727bd4620283a5a085988bfdbf7184ee9d841b0c
ArsenyCoder <114302368+ArsenyCoder@users.noreply.github.com>
2022-11-02 11:15:59 +0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -22,12 +22,13 @@ $('#about-me-link').on('click',function(e) {
 $('#open-video').on('click',function(e) {
   e.preventDefault();
   $('body,html').animate({ scrollTop: $('#main-video-container').offset().top - $('#nav').height() - 10 - ($(window).height() - $('#main-video-container').height() - $('#nav').height()) / 2},1500);
-  window.setTimeout(OpenMainVideo, 1500);
+  window.setTimeout(OpenMainVideo, 1800);
 });
+
 
 $('#open-map-popup').on('click',function(e) {
   e.preventDefault();
-  $('body,html').animate({ scrollTop: $('#main-map-id').offset().top - $('#nav').height() - ($(window).height() - $('#main-map-id').height() - $('#nav').height() / 2) / 2},650);
+  $('body,html').animate({ scrollTop: $('#registration-card').offset().top - $('#nav').height() - ($(window).height() - $('#registration-card').height() - $('#nav').height() / 2) / 2},1500);
 });
 
 $('#about-me-id-link').on('click',function(e) {
@@ -38,4 +39,9 @@ $('#about-me-id-link').on('click',function(e) {
 $('#little-about-me-is').on('click',function(e) {
   e.preventDefault();
   alert('Я-репетитор');
+});
+
+
+$('#open-about-popup').on('click',function(e) {
+    alert($('#nav').height());
 });
